12dbf8a939e2c7c7124c337d45b93f1872256e0b
JesusAlfredoCobosValencia <zS24013401@estudiantes.uv.mx>
2026-05-08 11:19:33 -0600

    tarea calculadora

--- javascript/javascript-02.js
+++ javascript/javascript-02.js
@@ -5,10 +5,9 @@ for (const key in botones) {
     if (Object.prototype.hasOwnProperty.call(botones, key)) {
         const boton = botones[key];
         if (boton.className != "operadores")
-        boton.addEventListener("click", pintar);
+            boton.addEventListener("click", pintar);
         else
-            boton.addEventListener("click",pintar2)
-        //console.log(boton);
+            boton.addEventListener("click", pintar2);
     }
 }
 
@@ -17,29 +16,49 @@ function pintar(e) {
     resultado.value += e.target.innerText;
 }
 
-let operadores = document.getElementsByClassName("operadores")
+let operadores = document.getElementsByClassName("operadores");
 for (const key in operadores) {
     if (Object.prototype.hasOwnProperty.call(operadores, key)) {
         const boton = operadores[key];
         boton.addEventListener("click", pintar2);
-        //console.log(boton);
     }
 }
 
+let prm1, prm2, operacion;
+
 function pintar2(e) {
     console.log(e.target.innerText);
-    if(e.target.innerText == "+")
-        suma()
+    if (e.target.innerText == "+" || e.target.innerText == "-" || e.target.innerText == "*" || e.target.innerText == "/") {
+        prm1 = resultado.value;
+        operacion = e.target.innerText;
+        resultado.value = "";
+    }
+    if (e.target.innerText == "Ce") {
+        resultado.value = "";
+    }
+    if (e.target.innerText == "=") {
+        igual();
+    }
 }
 
-let prm1
-let prm2
-function suma (){
-    prm1 = resultado.value
-    resultado.value = ""
-}
-
-function igual(){
-    prm2 = resultado.value
-    resultado.value = parseInt(prm1) + parseInt(prm2)
+function igual() {
+    prm2 = resultado.value;
+    switch (operacion) {
+        case "+":
+            resultado.value = parseFloat(prm1) + parseFloat(prm2);
+            break;
+        case "-":
+            resultado.value = parseFloat(prm1) - parseFloat(prm2);
+            break;
+        case "*":
+            resultado.value = parseFloat(prm1) * parseFloat(prm2);
+            break;
+        case "/":
+            if (prm2 == "0") {
+                resultado.value = "Error";
+            } else {
+                resultado.value = parseFloat(prm1) / parseFloat(prm2);
+            }
+            break;
+    }
 }
