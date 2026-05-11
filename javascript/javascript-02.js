b8066a14dfb415ac90e1c94a30d69af522a4c3c3
JesusAlfredoCobosValencia <zS24013401@estudiantes.uv.mx>
2026-05-11 11:25:54 -0600

    calculadora iniciaal

--- javascript/javascript-02.js
+++ javascript/javascript-02.js
@@ -1,64 +1,69 @@
-let resultado = document.getElementById("resultado");
-let botones = document.getElementsByTagName("button");
-
+let resultado = document.getElementById("resultado")
+let botones = document.getElementsByTagName("button")
 for (const key in botones) {
     if (Object.prototype.hasOwnProperty.call(botones, key)) {
         const boton = botones[key];
         if (boton.className != "operadores")
-            boton.addEventListener("click", pintar);
+            boton.addEventListener("click", pintar)
         else
-            boton.addEventListener("click", pintar2);
+            boton.addEventListener("click", operadores)
+        // console.log(boton)
     }
 }
 
 function pintar(e) {
-    console.log(e.target.innerText);
-    resultado.value += e.target.innerText;
+    console.log(e.target.innerText)
+    resultado.value += e.target.innerText
 }
 
-let operadores = document.getElementsByClassName("operadores");
-for (const key in operadores) {
-    if (Object.prototype.hasOwnProperty.call(operadores, key)) {
-        const boton = operadores[key];
-        boton.addEventListener("click", pintar2);
-    }
+function operadores(e) {
+    let operador = e.target.innerText
+    if (operador == "=")
+        igual()
+    else
+        almacena_parametro(operador)
 }
 
-let prm1, prm2, operacion;
-
-function pintar2(e) {
-    console.log(e.target.innerText);
-    if (e.target.innerText == "+" || e.target.innerText == "-" || e.target.innerText == "*" || e.target.innerText == "/") {
-        prm1 = resultado.value;
-        operacion = e.target.innerText;
-        resultado.value = "";
-    }
-    if (e.target.innerText == "Ce") {
-        resultado.value = "";
-    }
-    if (e.target.innerText == "=") {
-        igual();
-    }
+let prm1
+let operacion
+function almacena_parametro(prm) {
+    // prm1 = resultado.value
+    parametros.push(resultado.value)
+    eventos.push(resultado.value)
+    resultado.value = ""
+    operacion = prm
+    eventos.push(operacion)
 }
 
 function igual() {
-    prm2 = resultado.value;
-    switch (operacion) {
-        case "+":
-            resultado.value = parseFloat(prm1) + parseFloat(prm2);
-            break;
-        case "-":
-            resultado.value = parseFloat(prm1) - parseFloat(prm2);
-            break;
-        case "*":
-            resultado.value = parseFloat(prm1) * parseFloat(prm2);
-            break;
-        case "/":
-            if (prm2 == "0") {
-                resultado.value = "Error";
-            } else {
-                resultado.value = parseFloat(prm1) / parseFloat(prm2);
-            }
-            break;
+    // prm2 = resultado.value
+    let variable = null
+    parametros.push(resultado.value)
+    eventos.push(resultado.value)
+    for (const key in parametros) {
+        if (Object.prototype.hasOwnProperty.call(parametros, key)) {
+            const prm = parametros[key];
+            if (operacion == "+")
+                variable += parseInt(prm)
+            if (operacion == "-")
+                if (variable == null)
+                    variable = parseInt(prm)
+                else
+                    variable -= parseInt(prm)
+            if (operacion == "*")
+                if (variable == null)
+                    variable = parseInt(prm)
+                else
+                    variable *= parseInt(prm)
+            if (operacion == "/")
+                if (variable == null)
+                    variable = parseInt(prm)
+                else
+                    variable /= parseInt(prm)
+        }
     }
+    resultado.value = variable
 }
+
+let parametros = []
+let eventos = []
